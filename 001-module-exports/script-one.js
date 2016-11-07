2c998de75f8fd109a1ce2c06c0f9c1edd06378dd
devatha k <k.n.devs@gmail.com>
2016-11-07 14:55:30 -0500

    added reference

---
+++ 001-module-exports/script-one.js
@@ -0,0 +1,12 @@
+module.exports.myNumber = 123;
+// same as above
+exports.myNumber = 123;
+
+
+
+exports.sayHi= function(somename) {
+	console.log(" Hi " + somename)
+};
+
+
+module.exports.weekdays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
---
+++ 001-module-exports/script-two.js
@@ -0,0 +1,6 @@
+var greetings = require("./script-one.js");
+
+console.log(greetings.myNumber);
+console.log(greetings.sayHi("girl"));
+
+
