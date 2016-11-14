8146dc2dfd72a9b973da46ce87d84e4543071dbd
devatha k <k.n.devs@gmail.com>
2016-11-13 22:44:40 -0500

    react basics

--- 006-react-related/004-react-scaffold/experiment-stuff/src/index.js
+++ 006-react-related/004-react-scaffold/experiment-stuff/src/index.js
@@ -7,3 +7,23 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello
+  </h1>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root-one')
+);
